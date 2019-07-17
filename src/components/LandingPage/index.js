4ac8d52d733de42eb9b06d73c484494a8183ce50
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-07-17 16:59:08 -0700

    remote added with landing component

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function LandingPage(){
+    return (
+        <>
+            <div className="home-wrapper">
+                <h1>Book Drop</h1>
+            </div>
+        </>
+    )
+
+}
+
+export default LandingPage;
+
+
+
