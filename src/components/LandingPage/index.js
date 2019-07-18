083f7edee8a7a5f46866bcff754dd3d6eb6a7412
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-07-18 14:23:04 -0700

    landing Page design update and buttons added

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -10,9 +10,18 @@ function LandingPage(){
             <Grid item sm={12} className="splashImg">
                 This is the splash banner
             </Grid>
-            <Grid item sm>
-                <Paper>
-                    pane 1
+
+            <Grid item sm={12}>
+                <Paper className="section-a-landing">
+                    <h1>Welcome to Book Drop! </h1>
+                    <p>In todays colleges, students are asked to pay ever-increasing costs
+                        for books for each class they take.  And bookstores are offering less and less
+                        to buy those books back.  Book Drop is a way for students to connect with each other
+                        to sell and purchase books without a middleman.
+                    </p>
+                    <p> Book Drop links you with students from university or universities near you.</p>
+                    <button className="jump-button">Find Books!</button>
+                    <button className="jump-button">List a Book!</button>
                 </Paper>
             </Grid>
             <Grid item sm>
