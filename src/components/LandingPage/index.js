68d1e26619c8f78eb23562c2014fce2a7a4d89a6
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-07-17 19:52:34 -0700

    splashImg added and sized

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,12 +1,26 @@
 import React from "react";
+import './style.css';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
 
 function LandingPage(){
     return (
-        <>
-            <div className="home-wrapper">
-                <h1>Book Drop</h1>
-            </div>
-        </>
+        <Grid container>
+            <Grid item sm={12} className="splashImg">
+                This is the splash banner
+            </Grid>
+            <Grid item sm>
+                <Paper>
+                    pane 1
+                </Paper>
+            </Grid>
+            <Grid item sm>
+                <Paper>
+                    pane 2
+                </Paper>
+            </Grid>
+        </Grid>
     )
 
 }
